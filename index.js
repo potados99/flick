448e0f1a0c859c4e042b7a850261a80518dce336
potados99 <potados99@gmail.com>
2023-05-08 13:35:26 +0900

    refactor: rename and refactor backend

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
-const process = require('process');
-
-// Handle SIGINT
-process.on('SIGINT', () => {
-    console.info("SIGINT Received, exiting...")
-    process.exit(0)
-})
-
-// Handle SIGTERM
-process.on('SIGTERM', () => {
-    console.info("SIGTERM Received, exiting...")
-    process.exit(0)
-})
-
-// Handle APP ERRORS
-process.on('uncaughtException', (error, origin) => {
-    console.log('----- Uncaught exception -----')
-    console.log(error)
-    console.log('----- Exception origin -----')
-    console.log(origin)
-})
-process.on('unhandledRejection', (reason, promise) => {
-    console.log('----- Unhandled Rejection at -----')
-    console.log(promise)
-    console.log('----- Reason -----')
-    console.log(reason)
-})
-
-const express = require('express');
-const RateLimit = require('express-rate-limit');
 const http = require('http');
+const express = require('express');
+const {limiter} = require('./lib/middleware.js');
+const FlickServer = require('./lib/FlickServer.js');
+const {handleProcessEvents} = require('./lib/handler.js');
 
-const limiter = RateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
-    message: 'Too many requests from this IP Address, please try again after 5 minutes.',
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+handleProcessEvents();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(limiter);
 
-// ensure correct client ip and not the ip of the reverse proxy is used for rate limiting on render.com
-// see https://github.com/express-rate-limit/express-rate-limit#troubleshooting-proxy-issues
 app.set('trust proxy', 5);
 
 app.use(express.static('public'));
@@ -57,287 +23,17 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 
-server.listen(port);
+new FlickServer(httpServer);
 
-const parser = require('ua-parser-js');
-const fetch = require('node-fetch');
+httpServer.listen(port);
 
-class SnapdropServer {
-    constructor() {
-        const WebSocket = require('ws');
-        this._wss = new WebSocket.Server({ server });
-        this._wss.on('connection', async (socket, request) => {
-            const peer = new Peer(socket, request);
-            await peer.getReady(request);
-
-            this._onConnection(peer)
-        });
-        this._wss.on('headers', (headers, response) => this._onHeaders(headers, response));
-
-        this._rooms = {};
-
-        console.log('Snapdrop is running on port', port);
-    }
-
-    _onConnection(peer) {
-        this._joinRoom(peer);
-        peer.socket.on('message', message => this._onMessage(peer, message));
-        this._keepAlive(peer);
-
-        // send displayName
-        this._send(peer, {
-            type: 'display-name',
-            message: {
-                displayName: peer.name.displayName,
-                deviceName: peer.name.deviceName
-            }
-        });
-    }
-
-    _onHeaders(headers, response) {
-        if (response.headers.cookie && response.headers.cookie.indexOf('peerid=') > -1) return;
-        response.peerId = Peer.uuid();
-        headers.push('Set-Cookie: peerid=' + response.peerId + "; SameSite=Strict; Secure");
-    }
-
-    _onMessage(sender, message) {
-        // Try to parse message
-        try {
-            message = JSON.parse(message);
-        } catch (e) {
-            return; // TODO: handle malformed JSON
-        }
-
-        switch (message.type) {
-            case 'disconnect':
-                this._leaveRoom(sender);
-                break;
-            case 'pong':
-                sender.lastBeat = Date.now();
-                break;
-        }
-
-        // relay message to recipient
-        if (message.to && this._rooms[sender.ip]) {
-            const recipientId = message.to; // TODO: sanitize
-            const recipient = this._rooms[sender.ip][recipientId];
-            delete message.to;
-            // add sender id
-            message.sender = sender.id;
-            this._send(recipient, message);
-        }
-    }
-
-    _joinRoom(peer) {
-        // if room doesn't exist, create it
-        if (!this._rooms[peer.ip]) {
-            this._rooms[peer.ip] = {};
-        }
-
-        // notify all other peers
-        for (const otherPeerId in this._rooms[peer.ip]) {
-            const otherPeer = this._rooms[peer.ip][otherPeerId];
-            this._send(otherPeer, {
-                type: 'peer-joined',
-                peer: peer.getInfo()
-            });
-        }
-
-        // notify peer about the other peers
-        const otherPeers = [];
-        for (const otherPeerId in this._rooms[peer.ip]) {
-            otherPeers.push(this._rooms[peer.ip][otherPeerId].getInfo());
-        }
-
-        this._send(peer, {
-            type: 'peers',
-            peers: otherPeers
-        });
-
-        // add peer to room
-        this._rooms[peer.ip][peer.id] = peer;
-    }
-
-    _leaveRoom(peer) {
-        if (!this._rooms[peer.ip] || !this._rooms[peer.ip][peer.id]) return;
-        this._cancelKeepAlive(this._rooms[peer.ip][peer.id]);
-
-        // delete the peer
-        delete this._rooms[peer.ip][peer.id];
-
-        peer.socket.terminate();
-        //if room is empty, delete the room
-        if (!Object.keys(this._rooms[peer.ip]).length) {
-            delete this._rooms[peer.ip];
-        } else {
-            // notify all other peers
-            for (const otherPeerId in this._rooms[peer.ip]) {
-                const otherPeer = this._rooms[peer.ip][otherPeerId];
-                this._send(otherPeer, { type: 'peer-left', peerId: peer.id });
-            }
-        }
-    }
-
-    _send(peer, message) {
-        if (!peer) return;
-        if (this._wss.readyState !== this._wss.OPEN) return;
-        message = JSON.stringify(message);
-        peer.socket.send(message, error => '');
-    }
-
-    _keepAlive(peer) {
-        this._cancelKeepAlive(peer);
-        const timeout = 30000;
-        if (!peer.lastBeat) {
-            peer.lastBeat = Date.now();
-        }
-        if (Date.now() - peer.lastBeat > 2 * timeout) {
-            this._leaveRoom(peer);
-            return;
-        }
-
-        this._send(peer, { type: 'ping' });
-
-        peer.timerId = setTimeout(() => this._keepAlive(peer), timeout);
-    }
-
-    _cancelKeepAlive(peer) {
-        if (peer && peer.timerId) {
-            clearTimeout(peer.timerId);
-        }
-    }
-}
-
-class Peer {
-    constructor(socket, request) {
-        // set socket
-        this.socket = socket;
-
-        // set remote ip
-        this._setIP(request);
-
-        // set peer id
-        this._setPeerId(request)
-        // is WebRTC supported ?
-        this.rtcSupported = request.url.indexOf('webrtc') > -1;
-
-        // Method call for setting name is moved inside to getReady()
-        // because it  requires a network call.
-
-        // for keepalive
-        this.timerId = 0;
-        this.lastBeat = Date.now();
-    }
-
-    async getReady(request) {
-        // set name
-        await this._setName(request);
-    }
-
-    _setIP(request) {
-        if (request.headers['x-forwarded-for']) {
-            this.ip = request.headers['x-forwarded-for'].split(/\s*,\s*/)[0];
-        } else {
-            this.ip = request.connection.remoteAddress;
-        }
-        // IPv4 and IPv6 use different values to refer to localhost
-        if (this.ip === '::1' || this.ip === '::ffff:127.0.0.1') {
-            this.ip = '127.0.0.1';
-        }
-    }
-
-    _setPeerId(request) {
-        if (request.peerId) {
-            this.id = request.peerId;
-        } else {
-            this.id = request.headers.cookie.replace('peerid=', '');
-        }
-    }
-
-    toString() {
-        return `<Peer id=${this.id} ip=${this.ip} rtcSupported=${this.rtcSupported}>`
-    }
-
-    async _setName(req) {
-        let ua = parser(req.headers['user-agent']);
-
-        let deviceName = '';
-
-        if (ua.os && ua.os.name) {
-            deviceName = ua.os.name.replace('Mac OS', 'Mac') + ' ';
-        }
-
-        if (ua.device.model) {
-            deviceName += ua.device.model;
-        } else {
-            deviceName += ua.browser.name;
-        }
-
-        if(!deviceName)
-            deviceName = 'Unknown Device';
-
-        /*const displayName = uniqueNamesGenerator({
-            length: 2,
-            separator: ' ',
-            dictionaries: [colors, animals],
-            style: 'capital',
-            seed: this.id.hashCode()
-        })*/
-
-        const response = await fetch('https://nickname.hwanmoo.kr/?format=json&count=1&max_length=8');
-        const body = await response.json();
-        const displayName = body.words[0];
-
-        this.name = {
-            model: ua.device.model,
-            os: ua.os.name,
-            browser: ua.browser.name,
-            type: ua.device.type,
-            deviceName,
-            displayName
-        };
-    }
-
-    getInfo() {
-        return {
-            id: this.id,
-            name: this.name,
-            rtcSupported: this.rtcSupported
-        }
-    }
-
-    // return uuid of form xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
-    static uuid() {
-        let uuid = '',
-          ii;
-        for (ii = 0; ii < 32; ii += 1) {
-            switch (ii) {
-                case 8:
-                case 20:
-                    uuid += '-';
-                    uuid += (Math.random() * 16 | 0).toString(16);
-                    break;
-                case 12:
-                    uuid += '-';
-                    uuid += '4';
-                    break;
-                case 16:
-                    uuid += '-';
-                    uuid += (Math.random() * 4 | 8).toString(16);
-                    break;
-                default:
-                    uuid += (Math.random() * 16 | 0).toString(16);
-            }
-        }
-        return uuid;
-    };
-}
+console.log('Flick is running on port', port);
 
 Object.defineProperty(String.prototype, 'hashCode', {
     value: function() {
-        let hash = 0, i, chr;
+        let hash =  0, i, chr;
         for (i = 0; i < this.length; i++) {
             chr   = this.charCodeAt(i);
             hash  = ((hash << 5) - hash) + chr;
@@ -346,5 +42,3 @@ Object.defineProperty(String.prototype, 'hashCode', {
         return hash;
     }
 });
-
-new SnapdropServer();
