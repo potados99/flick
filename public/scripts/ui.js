5933a612be213901a4b09ab10390993aa9614b59
potados99 <potados99@gmail.com>
2023-05-23 08:24:59 +0900

    fix: 드래그 오버레이 height을 100%으로 주면 스크롤이 생기는 문제 해결

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -644,28 +644,3 @@ document.body.onclick = e => { // safari hack to fix audio
   if (!(/.*Version.*Safari.*/.test(navigator.userAgent))) return;
   blop.play();
 }
-
-function isElectron() {
-  // Renderer process
-  if (typeof window !== 'undefined' && typeof window.process === 'object' && window.process.type === 'renderer') {
-    return true;
-  }
-
-  // Main process
-  if (typeof process !== 'undefined' && typeof process.versions === 'object' && !!process.versions.electron) {
-    return true;
-  }
-
-  // Detect the user agent when the `nodeIntegration` option is set to true
-  if (typeof navigator === 'object' && typeof navigator.userAgent === 'string' && navigator.userAgent.indexOf('Electron') >= 0) {
-    return true;
-  }
-
-  return false;
-}
-
-window.onload = () => {
-  if (isElectron()) {
-    document.querySelector('x-title').setAttribute('show', 1);
-  }
-}
